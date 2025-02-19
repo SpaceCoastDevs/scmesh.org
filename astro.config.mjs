5dc2148476e166e8037b9659e7a9c30d34af3663
Gil Creque <gil@codecraftworks.com>
2025-02-19 18:07:31 -0500

    Base scmesh.org site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,33 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "🛸 Space Coast Meshtastic",
+      social: {
+        github: "https://github.com/SpaceCoastDevs/scmesh.org",
+      },
+      //   sidebar: [
+      //     {
+      //       label: "Guides",
+      //       items: [
+      //         // Each item here is one entry in the navigation menu.
+      //         { label: "Example Guide", slug: "guides/example" },
+      //       ],
+      //     },
+      //     {
+      //       label: "Reference",
+      //       autogenerate: { directory: "reference" },
+      //     },
+      //   ],
+      logo: {
+        src: "./src/assets/meshtastic.svg",
+      },
+      favicon: "./src/assets/meshtastic.svg",
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+    }),
+  ],
 });
