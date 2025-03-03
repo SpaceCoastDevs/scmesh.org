7196a34ee036160a3893f690eb1251148d33091f
Gil Creque <gil@codecraftworks.com>
2025-03-03 14:39:19 -0500

    Switched to PNG favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,25 @@ export default defineConfig({
       logo: {
         src: "./src/assets/scmesh-ufo.svg",
       },
-      favicon: "/scmesh-ufo.svg",
+      favicon: "/scmesh-ufo.png",
+      head: [
+        // Add ICO favicon fallback for Safari.
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "/favicon.ico",
+            sizes: "32x32",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        },
+      ],
       customCss: [
         // Relative path to your custom CSS file
         "./src/styles/custom.css",
